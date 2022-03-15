4cda95921d80966463e662c7543e1dcb9364015c
alina-mdhr <manandharalina170@gmail.com>
2022-03-15 14:50:56 +0545

    first commit

---
+++ meetups/admin.py
@@ -0,0 +1,17 @@
+from csv import list_dialects
+from turtle import title
+from django.contrib import admin
+from .models import Meetup,Location, Participant
+# Register your models here.
+
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title','date','location')
+    list_filter = ('location','date')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
+admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
+admin.site.register(Participant)
---
+++ meetups/urls.py
@@ -0,0 +1,8 @@
+from multiprocessing.spawn import import_main_path
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('meetups/',views.index,name='all-meetups' ),#our-domain.com/meetups
+    path('meetups/<slug:meetup_slug>',views.meetup_details,name='meetup-detail')
+]
